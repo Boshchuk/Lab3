7c32182bf60f76ca18e03eb3cb99eaf6e6fc9080
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2016-01-24 05:52:00 +0300

    -lab 3 finished

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -3,140 +3,26 @@
 #include <iostream>
 #include <vector>
 
-/*
-const int maxm = 5;
-const int maxn = 4;
-
-
-
-int f1[maxm][maxn];
-int f2[maxm][maxn];
-
-int besta [maxn];
-int n = 4, m = 5;
-
-
-int min( int a, int b, int c) 
-{
-	
-	if ((a <= b) && (a <= c)) {
-		return a;
-	}
-		
-	else
-	{
-		if ((b <= a) && (b <= c))
-		{
-			return	b;
-		}
-		else
-		 {
-			 if ((c <= b) && (c <= a))
-				 return c;
-		 }
-			
-		}
-}
-
-int minn(int a, int b, int c)
-{
-	if ((a <= b) && (a <= c))
-	{
-		return  -1;
-	}
-	else
-	{
-		if ((b <= a) && (b <= c))
-		{
-			return  0;
-		}
-			 else
-			 {
-				 if ( (c <= b) && (c <= a))
-				 {
-				    return 1;
-				 }
-					 
-			 }
-			
-	}
-	
-	
-		
-}
-
-
-
-
-int mx(int a)
-{
-	if (a<1) return  1;
-	if (a>n) return n;
-}
-
-
-
-
-
-
-
-int main()
-{
-
-	int a[maxm][maxn] =
-	{
-		{ 4,  1, 6, 8 },
-		{ 1,  1, 6, 9 },
-		{ 10, 5, 1, 9 },
-		{ 7,  6, 1, 9, },
-		{ 10, 2, 6, 2, },
-	};
-
-	for (auto j = 1; j < m ; j++)
-	{
-		for (auto i = 0; i < n; i ++)
-		{
-			a[j] [i] = min(a[j - 1][ mx(i - 1)], a[j - 1][ i], a[j - 1][ mx(i + 1)]) + a[j][ i];
-		}
-	}
-
-
-auto b = 0;
-for (auto i = 0; i < n; i++)
-{
-	if (a[m][ b]>a[m][ i])
-		b  = i;
-}
-
-int bestst = a[m][b];
-
-besta[m] = b;
-for (auto i = m ; i>0; i--)
-{
-	besta[i] = mx(besta[i + 1] + minn(a[i][ mx(besta[i + 1] - 1)], a[i][ mx(besta[i + 1])], a[i][ mx(besta[i + 1] + 1)]));
-}
-
-std::cout << bestst  << std::endl;
-
- for (auto i = 0; i < m; i++)
-{
-   std::cout <<  a[i][ mx(besta[i])] << ' ' ;
-}
-
-
-	return 0;
-}
-*/
-
-
 using namespace std;
-const int n = 4;
-const int m = 5;
+
+const int size_n = 4;
+const int size_m = 5;
 
 const int V = 20; // 4*5
 
+struct ResultItem
+{
+	int straph;
+	vector<int> trace;
+};
+
+struct Result
+{
+	ResultItem ForEachStr[size_n-1];
+};
+
 //алгоритм Дейкстры
-void Dijkstra(int** GR, int st)
+Result DijkstraWithAction(int** GR, int st, int stratValue, bool part1)
 {
 	int distance[V], count, index, i, u, m = st + 1;
 
@@ -149,6 +35,10 @@ void Dijkstra(int** GR, int st)
 		visited[i] = false;
 	}
 	distance[st] = 0;
+
+	trace[st].push_back(stratValue);
+
+
 	for (count = 0; count<V - 1; count++)
 	{
 		int min = INT_MAX;
@@ -167,38 +57,98 @@ void Dijkstra(int** GR, int st)
 			if (!visited[i] && GR[u][i] && distance[u] != INT_MAX &&
 				distance[u] + GR[u][i] < distance[i])
 			{
-			/*	trace[i].push_back(distance[u]);
-				trace[i].push_back(GR[u][i]);*/
+				//trace[i].push_back(distance[u]);
+
+				for (auto it = trace[u].begin(); it != trace[u].end(); ++it)
+				{
+					trace[i].push_back(*it);
+				}
+				trace[i].push_back(GR[u][i]);
 				distance[i] = distance[u] + GR[u][i];
 			}
 		}
 	}
-	cout << "Стоимость пути из начальной вершины до остальных:\t\n";
-	for (i = 0; i < V; i++) {
-		if (distance[i] != INT_MAX)
-		{
-			cout << m << " > " << i + 1 << " = " << distance[i] << endl;
 
-			//for (auto it = trace[i].begin(); it != trace[i].end(); ++it)
-			//{
-			//	cout << *it << " ";
-			//}
-			//cout << endl;
-
-		}
-		else
+	if (part1)
+	{
+		int arMin[size_n - 1];
+		int dis[size_n - 1];
+		int l = 0;
+	
+		for (int i = size_m; i < V;)
 		{
-			cout << m << " > " << i + 1 << " = " << "маршрут недоступен" << endl;
+			int t_MIn= INT_MAX;
+			arMin[l] = i;
+			
+			for (int j = 0; j < size_m; j++)
+			{
+				if (distance[i+j] <= t_MIn)
+				{
+					t_MIn = distance[i+j];
+					arMin[l] = i + j;
+					dis[l] =  distance[i + j];
+				}
+			}
+			i = i + size_m;
+			l++;
 		}
+
+		Result res;
+		//res.Pos = 
+		for (int i = 0; i < size_n - 1;i++)
+		{
+			int number = arMin[i];
+		/*	cout << "Pos of min stroku in stroka:" << i + 2 << "      " <<arMin[i] << endl;
+			cout << "Put:";
+			
+			for (auto it = trace[number].begin(); it != trace[number].end(); ++it)
+			{
+				cout << *it << " ";
+			}
+			cout << endl;*/
+
+			
+			res.ForEachStr[i].trace = trace[number];
+			res.ForEachStr[i].straph = dis[i];
+		}
+
+		return res;
+
 	}
+	else
+	{
+		cout << "Стоимость пути из начальной вершины до остальных:\t\n";
+		for (i = 0; i < V; i++)
+		{
+			if (distance[i] != INT_MAX)
+			{
+				cout << m << " > " << i + 1 << " = " << distance[i] << endl;
+
+				for (auto it = trace[i].begin(); it != trace[i].end(); ++it)
+				{
+					cout << *it << " ";
+				}
+				cout << endl;
+
+			}
+			else
+			{
+				cout << m << " > " << i + 1 << " = " << "маршрут недоступен" << endl;
+			}
+		}
+		Result r;
+		return 	r;
+	}
+
+
 }
 
 bool IsInRange(int cI,int cJ)
 {
-	return (cI >= 0 && cI < m) && (cJ>=0 && cJ < n);
+	return (cI >= 0 && cI < size_m) && (cJ>=0 && cJ < size_n);
 }
 
-int** GenereateSmegnostGraph(int matrix[n][m])
+int** GenereateSmegnostGraph(int matrix[size_n][size_m])
 {
 	int** table = new int*[V];
 	for (int i = 0; i < V; ++i)
@@ -218,8 +168,8 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 	{
 		// tranlsate index
 		
-		int j = index / m;
-		int i = index - j*m;
+		int j = index / size_m;
+		int i = index - j*size_m;
 
 		// check can move 8 direction 
 		// using ULITKA NOTATION:
@@ -228,8 +178,6 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 			8 x 4
 			7 6 5
 		*/
-
-
 
 		//check 1
 
@@ -241,7 +189,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index - m - 1;
+			int tV = index - size_m - 1;
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -254,7 +202,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index - m;
+			int tV = index - size_m;
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -267,7 +215,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index - (m - 1);
+			int tV = index - (size_m - 1);
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -293,7 +241,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index + m + 1;
+			int tV = index + size_m + 1;
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -306,7 +254,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index + m;
+			int tV = index + size_m;
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -319,7 +267,7 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 		{
 			//find number
 
-			int tV = index + (m-1);
+			int tV = index + (size_m-1);
 
 			//write into smejnost
 			table[index][tV] = matrix[cJ][cI];
@@ -342,15 +290,13 @@ int** GenereateSmegnostGraph(int matrix[n][m])
 	return table;
 }
 
-
-
 //главная функция
 void main()
 {
 
 	setlocale(LC_ALL, "Rus");
 
-	int strafMatrix[n][m] = {
+	int strafMatrix[size_n][size_m] = {
 		{3, 2, 8, 6, 4},
 		{4, 7, 12,9, 1},
 		{55,8, 3, 2, 8},
@@ -361,20 +307,72 @@ void main()
 
 	int start;
 	
-	
-	for (int i = 0; i < V; ++i)
+	// рисование смежности для проверки
+	/*for (int i = 0; i < V; ++i)
 	{
 		for (int j = 0; j < V; j++)
 		{
 			cout << GR[i][j] << " ";
 		}
 		cout << endl;
+	}*/
+
+	Result theMin;
+	for (int i=0; i < size_n - 1; i++)
+	{
+		theMin.ForEachStr[i].straph = INT_MAX;
 	}
 
-	cout << "Начальная вершина >> "; cin >> start;
-	Dijkstra(GR, start - 1);
 
-	GR = nullptr;
+	for (int i = 0; i < size_m; i ++)
+	{
+		int jk = i  / size_m;
+		int ik = i  - jk*size_m;
+		Result res = DijkstraWithAction(GR, i, strafMatrix[jk][ik], true);
 
-	system("pause>>void");
+		for (int ii = 0; ii < size_n - 1; ii++)
+		{
+			if (res.ForEachStr[ii].straph <=theMin.ForEachStr[ii].straph)
+			{
+				if (res.ForEachStr[ii].straph == theMin.ForEachStr[ii].straph)
+				{
+					if( res.ForEachStr[ii].trace.front() < theMin.ForEachStr[ii].trace.front())
+					{
+						theMin.ForEachStr[ii].straph = res.ForEachStr[ii].straph;
+						theMin.ForEachStr[ii].trace = res.ForEachStr[ii].trace;
+					}
+				}
+				else
+				{
+					theMin.ForEachStr[ii].straph = res.ForEachStr[ii].straph;
+					theMin.ForEachStr[ii].trace = res.ForEachStr[ii].trace;
+				}
+			}
+		}
+	}
+
+	
+
+	for (int line = 0; line < size_n - 1; line++)
+	{
+		cout << "Минимальный штраф c 1 до строки #"<< line+2 << ": "<<theMin.ForEachStr[line].straph + theMin.ForEachStr[line].trace.front() <<endl;
+		cout << "Маршрут штрафов:";
+		for (auto it = theMin.ForEachStr[line].trace.begin(); it != theMin.ForEachStr[line].trace.end(); ++it)
+		{
+			cout << *it << " ";
+		}
+		cout << endl;
+	}
+
+
+	cout << "Начальная вершина >> ";std::cin >> start;
+
+	int j = start - 1 / size_m;
+	int i = start -1 - j*size_m;
+
+	Result res =  DijkstraWithAction(GR, start - 1, strafMatrix[j][i],false);
+
+	//GR = nullptr;
+
+	std::system("pause>>void");
 }
